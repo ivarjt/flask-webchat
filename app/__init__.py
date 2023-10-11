07f30cd766f344c77fd696dc1fc557b2b41f0c53
ivarjt <demongoat@scuba.se>
2023-10-11 14:52:09 +0200

    Started with the login system and created manage_db.py tool - Ivar & Emil

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, g
 from flask_socketio import SocketIO
+import sqlite3
 
 app = Flask(__name__)
+
+DATABASE = 'database.db'
+
 app.config['SECRET_KEY'] = 'secret_key'
 socketio = SocketIO(app)
 
-from app import views, admin_views
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+from app import views, admin_views, login_views
