c24dc7393968f84076caf99d5937f9248750bcac
ivarjt <demongoat@scuba.se>
2023-10-18 15:14:06 +0200

    Changed so that the username in the chatroom is the currently logged in user

--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
-from app import app, socketio
-from flask import render_template
+from app import app, socketio, db, login_manager
+from flask import render_template, redirect, url_for
 from flask_socketio import SocketIO, send
-
+from flask_login import login_user, login_required, current_user
+from .models import User
+        
+# Chat room
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", username=current_user.username)
 
 @socketio.on('message')
 def handle_message(data):
