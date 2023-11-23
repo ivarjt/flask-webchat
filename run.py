85bcf7cc5dd211aa9c6f863e4a8236366ac0b6e7
ivarjt <demongoat@scuba.se>
2023-11-23 19:21:33 +0100

    #40 Made database print red color, added some comments in run.py file and made two constants in __init__.py file

--- run.py
+++ run.py
@@ -1,12 +1,20 @@
 import os
-from app import app, socketio, db
+from app import app, socketio, db, RUN_IN_DEBUG_MODE, HOST
+from colorama import Fore, Style
 
+# Check if the script is being run directly
 if __name__ == "__main__":
+    # Define path to database file
     database_file = os.path.join(app.instance_path, "db.sqlite3")
 
+    # Check if database file already exists
     if not os.path.exists(database_file):
-        print("Database file does not exist. Creating a new one...")
+        # Alerts the user that the database file does not exist and is being created
+        print(f"{Fore.RED}Database file does not exist. Creating a new one...{Style.RESET_ALL}")
+        
+        # Create the database file
         with app.app_context():
             db.create_all()
             
-    socketio.run(app, debug=True, host="0.0.0.0")
+    # Run the Flask application with SocketIO
+    socketio.run(app, debug=RUN_IN_DEBUG_MODE, host=HOST)
