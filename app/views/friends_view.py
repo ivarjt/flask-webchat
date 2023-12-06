9d88cec4df3f4406b99c8a318598a461a55895f6
Emil-andersson1 <milemanen@gmail.com>
2023-12-06 11:18:37 +0100

    #36 Added error handling for friend system - Emil

--- app/views/friends_view.py
+++ app/views/friends_view.py
@@ -1,22 +1,42 @@
 from app import app
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
 from ..forms import FriendRequestForm
 from ..models import Friendship, User
 from flask_login import current_user
+from wtforms.validators import ValidationError
 
 @app.route("/send_friend_request", methods=["GET", "POST"])
 def send_friend_request():
-    # Create a form instance
     form = FriendRequestForm()
-    
-    # Process the form data if the request method is POST
+
     if form.validate_on_submit():
-        friendship_instance = Friendship()
         receiver_id = Friendship.convert_username_to_user_id(form.username.data)
-        friendship_instance.send_friend_request(sender_id=current_user.id, receiver=User.query.get(receiver_id))
-        return redirect(url_for("success"))
+        receiver = User.query.get(receiver_id)
+
+        if receiver is not None:  # Ensure the receiver exists
+            if current_user.id == receiver_id:
+                flash("You cannot send a friend request to yourself.", "error")
+            elif Friendship.query.filter(
+                (Friendship.sender_id == current_user.id) &
+                (Friendship.receiver_id == receiver_id) &
+                (Friendship.status == 'pending')
+            ).first():
+                flash("You have already sent a friend request to this person.", "error")
+            elif Friendship.query.filter(
+                ((Friendship.sender_id == receiver_id) & (Friendship.receiver_id == current_user.id)) |
+                ((Friendship.sender_id == current_user.id) & (Friendship.receiver_id == receiver_id))
+            ).filter(Friendship.status == 'accepted').first():
+                flash("You are already friends with this person.", "error")
+            else:
+                friendship_instance = Friendship()
+                friendship_instance.send_friend_request(sender_id=current_user.id, receiver=receiver)
+                flash("Friend request sent.", "success")
+                return redirect(url_for("success"))
+        else:
+            flash("This username does not exist. Please try again.", "error")
 
     return render_template("friends/send_friend_request.html", form=form)
+
 
 @app.route('/accept_friend_request/<int:request_id>', methods=['POST'])
 def accept_friend_request(request_id):
@@ -32,6 +52,7 @@ def accept_friend_request(request_id):
     # Redirect the user to the appropriate page
     return redirect(url_for('home'))
 
+
 @app.route('/reject_friend_request/<int:request_id>', methods=['POST'])
 def reject_friend_request(request_id):
     # Call the reject_friend_request function
@@ -46,14 +67,17 @@ def reject_friend_request(request_id):
     # Redirects the user
     return redirect(url_for('home'))
 
+
 @app.route("/handle_friend_request")
 def handle_friend_request():
     incoming_requests = current_user.get_incoming_friend_requests()
     return render_template("friends/handle_friend_request.html", incoming_requests=incoming_requests)
 
-@app.route("/success") #FIXME: temporary
+
+@app.route("/success")  # FIXME: temporary
 def success():
     return render_template("friends/success.html")
+
 
 @app.route("/list_friends")
 def list_friends():
@@ -62,9 +86,9 @@ def list_friends():
     # Render the template with the list of friends
     return render_template("friends/remove_friend.html", friends=friends)
 
+
 @app.route("/remove_friend/<string:friend>", methods=["POST"])
 def remove_friend(friend):
-
     # Convert the username to a user ID
     friend = Friendship.convert_username_to_user_id(friend)
 
