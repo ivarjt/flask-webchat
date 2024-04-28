15f909983ad6873a791d39a9310b52f6393b9bf9
ivarjt <demongoat@scuba.se>
2024-04-28 17:33:01 +0200

    #21 fixed bug

--- app/views/chat_views.py
+++ app/views/chat_views.py
@@ -1,12 +1,12 @@
 from app import app, socketio, db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from flask_socketio import rooms, emit, join_room, leave_room
 from flask_login import current_user
 from ..models import Room, Friendship, Message
 
-@app.route("/chat1/<int:room_id>")
-def chat1(room_id):  
-    return render_template("chat/chat1.html", username=current_user.username, room_id=room_id)
+@app.route("/chat/<int:room_id>")
+def chat(room_id):  
+    return render_template("chat/chat.html", username=current_user.username, room_id=room_id)
 
 @app.route("/create_room/<string:user2_name>", methods=["POST"])
 def create_room(user2_name):
@@ -16,15 +16,13 @@ def create_room(user2_name):
     existing_room = Room.room_exists(user1_id, user2_id)
     if existing_room:
         # Room already exists
-            return redirect(url_for('chat1', room_id=existing_room.id))
-
-
+            return redirect(url_for('chat', room_id=existing_room.id))
     
     room = Room(user1_id=user1_id, user2_id=user2_id)
     db.session.add(room)
     db.session.commit()
     
-    return redirect(url_for("chat1", room_id=room.id))
+    return redirect(url_for("chat", room_id=room.id))
 
 
 @socketio.on('connect')
@@ -64,9 +62,8 @@ def on_join(data):
     
     # Emit each message to the user who joined the room
     for message in messages:
-        emit("message", {"username": message.user.username, "message": message.message}, to=room)
+        emit("message", {"username": message.user.username, "message": message.message}, to=request.sid)
 
-    
 @socketio.on('leave')
 def on_leave(data):
     username = data.get('username')
